000c8d2d190cf16c8e4ac6ff89893b29690675da
ananeke <ananeke@gmail.com>
2017-04-21 21:19:05 +0200

    Code optimize

--- Zadanie_13-Napisy.py
+++ Zadanie_13-Napisy.py
@@ -3,66 +3,42 @@ from tkinter import *
 napis = ''
 
 
-def sprawdz(z):
-    global e1, e2, e3, t, napis
+def znaki():
+    global e1, e2, e3, napis
     ile_liczb = 0
     ile_liter = 0
+    e1.config(state='normal')
+    e1.delete(0, END)
+    e1.insert(0, 'Znaków:' + str(len(napis)))
+    e1.config(state='readonly')
+    e2.config(state='normal')
+    e2.delete(0, END)
+    for i in napis:
+        if i.isalpha():
+            ile_liter += 1
 
+    e2.insert(0, 'Liter:' + str(ile_liter))
+    e2.config(state='readonly')
+
+    e3.config(state='normal')
+    e3.delete(0, END)
+
+    for i in napis:
+        if i.isnumeric():
+            ile_liczb += 1
+
+    e3.insert(0, 'Cyfr:' + str(ile_liczb))
+    e3.config(state='readonly')
+
+
+def sprawdz(z):
+    global napis
     if z.keycode == 8:
-
         napis = napis[:-1]
-
-        e1.config(state='normal')
-        e1.delete(0, END)
-        e1.insert(0, 'Znaków:' + str(len(napis)))
-        e1.config(state='readonly')
-
-        e2.config(state='normal')
-        e2.delete(0, END)
-        for i in napis:
-            if i.isalpha():
-                ile_liter += 1
-
-        e2.insert(0, 'Liter:' + str(ile_liter))
-        e2.config(state='readonly')
-
-        e3.config(state='normal')
-        e3.delete(0, END)
-
-        for i in napis:
-            if i.isnumeric():
-                ile_liczb += 1
-
-        e3.insert(0, 'Cyfr:' + str(ile_liczb))
-        e3.config(state='readonly')
+        znaki()
     else:
-
         napis = napis + z.char
-
-        e1.config(state='normal')
-        e1.delete(0, END)
-        e1.insert(0, 'Znaków:' + str(len(napis)))
-        e1.config(state='readonly')
-
-        e2.config(state='normal')
-        e2.delete(0, END)
-        for i in napis:
-            if i.isalpha():
-                ile_liter += 1
-
-        e2.insert(0, 'Liter:' + str(ile_liter))
-        e2.config(state='readonly')
-
-        e3.config(state='normal')
-        e3.delete(0, END)
-
-        for i in napis:
-            if i.isnumeric():
-                ile_liczb += 1
-
-        e3.insert(0, 'Cyfr:' + str(ile_liczb))
-        e3.config(state='readonly')
-
+        znaki()
 
 ok = Tk()
 
